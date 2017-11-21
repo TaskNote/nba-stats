59537714ddd7030fce90e01ae259bce166425841
Tomas McCandless <tomas.mccandless@gmail.com>
2017-11-20 22:42:45 -0800

    jsoup example

---
+++ src/test/scala/org/nbastats/scraper/ScraperSpec.scala
@@ -0,0 +1,21 @@
+package org.nbastats.scraper
+
+import org.junit.Test
+import org.nbastats.BaseSpec
+import org.nbastats.model.CareerSummary
+
+class ScraperSpec extends BaseSpec {
+
+
+  /** Checks that we can scrape tim duncans career summary. */
+  @Test
+  def duncan(): Unit = {
+    val timmy: CareerSummary = Scraper.scrapeCareerSummary("https://www.basketball-reference.com/players/d/duncati01.html")
+
+    timmy.name should be ("Tim Duncan")
+    timmy.games should be (1392)
+    timmy.pointsPerGame should be (19.0)
+    timmy.reboundsPerGame should be (10.8)
+    timmy.assistsPerGame should be (3.0)
+  }
+}
